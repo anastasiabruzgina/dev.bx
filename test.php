626e24294a0a7d28fbc220a1eec733e0b01c9904
anastasiabruzgina <73475424+anastasiabruzgina@users.noreply.github.com>
2020-11-11 18:18:38 +0200

    тест к функции ReadFromConsole

--- test.php
+++ test.php
@@ -1,56 +1,76 @@
-<?php
+echo readFromConsoleTest();
 
 require("./readfromconsole.php");
 
 function readFromConsoleTest()
 {
 	$result = readFromConsole('true');
-	if($result = 'true') {
-		echo ') Результат: '. $result . PHP_EOL . 'Ожидалось: true' . PHP_EOL . ($result === 'true' ? 'Test is passed'.PHP_EOL : 'Test is failed'.PHP_EOL);
-	} else {
-		echo 'Test is failed';
-	}
-}
+	echo 'Результат: '.
+		 $result.
+		 PHP_EOL.
+		 'Ожидалось: true'.
+		 PHP_EOL.
+		 ($result === true ? 'Test is passed'.PHP_EOL.PHP_EOL : 'Test is failed'.PHP_EOL.PHP_EOL);
 
-{
 	$result = readFromConsole('false');
-	if($result = 'false') {
-		echo 'Результат: '. $result . PHP_EOL . 'Ожидалось: false' . PHP_EOL . ($result === 'false' ? 'Test is passed'.PHP_EOL . PHP_EOL : 'Test is failed'.PHP_EOL);
-	} else {
-		echo 'Test is failed';
-	}
-}
-{
+	echo 'Результат: '.
+		 $result.
+		 PHP_EOL.
+		 'Ожидалось: false'.
+		 PHP_EOL.
+		 ($result === false ? 'Test is passed'.PHP_EOL.PHP_EOL : 'Test is failed'.PHP_EOL.PHP_EOL);
+
 	$result = readFromConsole('!stop');
-	if($result = 'null') {
-		echo 'Результат: '. $result . PHP_EOL . 'Ожидалось: null' . PHP_EOL . ($result === 'null' ? 'Test is passed'.PHP_EOL . PHP_EOL : 'Test is failed'.PHP_EOL);
-	} else {
-		echo 'Test is failed';
-	}
-}
-	{
+	echo 'Результат: '.
+		 $result.
+		 PHP_EOL.
+		 'Ожидалось: null'.
+		 PHP_EOL.
+		 ($result === null ? 'Test is passed'.PHP_EOL.PHP_EOL : 'Test is failed'.PHP_EOL.PHP_EOL);
+
 	$result = readFromConsole('1.3');
-	if($result = '1.3') {
-		echo 'Результат: '. $result . PHP_EOL . 'Ожидалось: 1.3' . PHP_EOL . ($result === '1.3' ? 'Test is passed'.PHP_EOL . PHP_EOL : 'Test is failed'.PHP_EOL);
-	} else {
-		echo 'Test is failed';
-	}
-}
-	{
+	echo 'Результат: '.
+		 $result.
+		 PHP_EOL.
+		 'Ожидалось: 1.3'.
+		 PHP_EOL.
+		 ($result === 1.3 ? 'Test is passed'.PHP_EOL.PHP_EOL : 'Test is failed'.PHP_EOL.PHP_EOL);
+
 	$result = readFromConsole('1');
-	if($result = '1') {
-		echo 'Результат: '. $result . PHP_EOL . 'Ожидалось: 1' . PHP_EOL . ($result === '1' ? 'Test is passed'.PHP_EOL . PHP_EOL : 'Test is failed'.PHP_EOL);
-	} else {
-		echo 'Test is failed';
-	}
-}
-	{
+	echo 'Результат: '.
+		 $result.
+		 PHP_EOL.
+		 'Ожидалось: 1'.
+		 PHP_EOL.
+		 ($result === 1 ? 'Test is passed'.PHP_EOL.PHP_EOL : 'Test is failed'.PHP_EOL.PHP_EOL);
+
 	$result = readFromConsole('test');
-	if($result = 'test') {
-		echo 'Результат: '. $result . PHP_EOL . 'Ожидалось: test' . PHP_EOL . ($result === 'test' ? 'Test is passed'.PHP_EOL . PHP_EOL : 'Test is failed'.PHP_EOL);
-	} else {
-		echo 'Test is failed';
-	}
+	echo 'Результат: '.
+		 $result.
+		 PHP_EOL.
+		 'Ожидалось: test'.
+		 PHP_EOL.
+		 ($result === 'test' ? 'Test is passed'.PHP_EOL.PHP_EOL : 'Test is failed'.PHP_EOL.PHP_EOL);
 }
 
 echo readFromConsoleTest();
+
+echo PHP_EOL.'Введите данные для проверки: '.PHP_EOL;
+$result = readFromConsole();
+
+echo 'Ожидалось: '.PHP_EOL;
+$expectResult = trim(fgets(STDIN));
+
+echo 'Результат: '.assertEquals($expectResult, $result).PHP_EOL;
+
+function assertEquals($expectResult, $result)
+{
+	if ($result == $expectResult)
+	{
+		return 'passed';
+	}
+	else
+	{
+		return 'failed';
+	}
+}
